e996e44bf0649dbcb9bc2f3447dd645dfd681318
sisiishereb <set38246@marymount.edu>
2020-09-26 14:53:33 -0400

    updating files

---
+++ freshprince/src/components/Jumbo/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container text-center">
+      <h1 className="display-4">Fresh Prince Clicky Game!</h1>
+      <p className="lead">
+        Click on any image to earn points, but once is enough...otherwise,
+        you're done!
+      </p>
+    </div>
+  </div>
+);
+export default Jumbotron;
